138d01bc8ed6dda932ba6226d712f1e8b5519d68
David Beckwith <thirdreplicator@gmail.com>
2023-03-16 15:04:45 +0800

    added /categories endpoint for product categories

--- src/load_products.js
+++ src/load_products.js
@@ -14,10 +14,17 @@ export const get_products = (req, resp) => {
     let { price, category_id } = obj
     price = parseFloat(price)
     let category = category_index[category_id]
-    return { ...obj, price, category }
+    return { ...obj, price }
   }
   pool.query('SELECT * FROM "Product"', (error, results) => {
     if (error) { throw error }
     resp.status(200).json(results.rows.map(modify))
+  })
+}
+
+export const get_categories = (req, resp) => {
+  pool.query('SELECT * FROM "Category"', (error, results) => {
+    if (error) { throw error }
+    resp.status(200).json(results.rows)
   })
 }
