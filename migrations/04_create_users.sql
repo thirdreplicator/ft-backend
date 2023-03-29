754bc390cf8da18ee3719fdfe9bdf4f91366841e
David Beckwith <thirdreplicator@gmail.com>
2023-03-29 16:02:43 +0800

    create users table.

---
+++ migrations/04_create_users.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "hashed_password" TEXT NOT NULL,
+    "phone_number" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "User_email_idx" ON "User"("email");
